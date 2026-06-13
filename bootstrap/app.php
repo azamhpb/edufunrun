61c01dde5bf88c489d425b2f5aa8e42723dc57e0
azamhpb <azamhpb@gmail.com>
2026-06-14 01:09:25 +0800

    tambah web route web-mejaclass

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
                 ->group(base_path('routes/web-guests.php'));
             Route::middleware('web')
                 ->group(base_path('routes/web-kaunterqr.php'));
+            Route::middleware('web')
+                ->group(base_path('routes/web-mejaclass.php'));
 
 
         }
